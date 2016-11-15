a3e541fc44d6d84c40f1b5507ae4b8e8cceaf302
Devon <djp460@nyu.edu>
2016-11-15 14:48:55 -0500

    Made scrolling more user-friendly.

--- src/controls/MouseWheelListener.java
+++ src/controls/MouseWheelListener.java
@@ -6,6 +6,8 @@ import platformermapdesigner.MapPane;
 
 public class MouseWheelListener implements EventHandler<ScrollEvent>{
     private final MapPane mp;
+    private int counter;
+    private final int threshold = 9;
     
     public MouseWheelListener(MapPane mp){
         this.mp = mp;
@@ -14,15 +16,23 @@ public class MouseWheelListener implements EventHandler<ScrollEvent>{
     @Override
     public void handle(ScrollEvent e) {
         if (e.getDeltaY() < 0){
-            //if the currentTile is less than the total number of tiles
-            if(mp.currentTile < mp.numTileRows*mp.numTileColumns-1){
-                mp.currentTile++;
+            counter++;
+            if (counter == threshold) {
+                counter = 0;
+                //if the currentTile is less than the total number of tiles
+                if(mp.currentTile < mp.numTileRows*mp.numTileColumns-1){
+                    mp.currentTile++;
+                }
             }
         }
         else if(e.getDeltaY() > 0){
-            //if the currentTile isn't the first tile
-            if (mp.currentTile > 0){
-                mp.currentTile--;
+            counter++;
+            if (counter == threshold) {
+                counter = 0;
+                //if the currentTile isn't the first tile
+                if (mp.currentTile > 0) {
+                    mp.currentTile--;
+                }
             }
         }
         mp.updateCurrentTile();
